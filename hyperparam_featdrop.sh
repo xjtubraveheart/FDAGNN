207fe74f7698f36dfd02d826344f3a99dc6f0e6b
muruochen <wyh9876@163.com>
2022-05-11 09:52:05 +0000

    删除文件 hyperparam_featdrop.sh

--- hyperparam_featdrop.sh
+++
@@ -1,8 +0,0 @@
-#conda activate Ada-gat
-# python main.py  --feat_drop=0 
-python main.py  --feat_drop=0.1 
-python main.py  --feat_drop=0.2  
-python main.py  --feat_drop=0.3 
-python main.py  --feat_drop=0.4 
-python main.py  --feat_drop=0.5 
-python main.py  --feat_drop=0.6  
