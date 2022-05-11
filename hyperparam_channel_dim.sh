aed91b59b10262de982c510db745ef78866f1b97
muruochen <wyh9876@163.com>
2022-05-11 09:51:50 +0000

    删除文件 hyperparam_channel_dim.sh

--- hyperparam_channel_dim.sh
+++
@@ -1,9 +0,0 @@
-#conda activate Ada-gat
-python main.py  --hidden=2048 
-python main.py  --hidden=1024  
-python main.py  --hidden=512 
-python main.py  --hidden=256 
-python main.py  --hidden=128 
-python main.py  --hidden=64 
-python main.py  --hidden=32 
-python main.py  --hidden=8 
